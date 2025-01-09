48af0f0fd91890d0b0fa1ad1c299d114e489e1d9
Peter Mahady <peader88@gmail.com>
2025-01-09 20:34:24 +0000

    add a telegram integration

--- bin/analyzeTrends.py
+++ bin/analyzeTrends.py
@@ -1,17 +1,30 @@
 # parse the 2024lego_eol_offers.json file
 import json
+import os
 from updateOffers import updateOffers
+from telethon import TelegramClient
 
 offerJsonFile = 'data/2024_lego_eol_offers.json'
-updateOffers(offerJsonFile)
+isSkipOfferUpdate = True
 
+async def sendResultMessage(resultMessage):
+    await client.send_message('me', resultMessage)
+
+api_id = os.getenv('telegram_api_id')
+api_hash = os.getenv('telegram_api_hash')
+client = TelegramClient('legotrender', api_id, api_hash)
+if(not isSkipOfferUpdate):
+    updateOffers(offerJsonFile)
+
+resultMessage = []
 with open(offerJsonFile, 'r') as f:
     json_data = json.load(f)
 
     for element in json_data:
         offers = element['offers'] 
         legosetid = element['legosetid']
-
+        if(len(offers)) <= 1:
+            continue
         if element['isRetired']:
             continue
         if offers[-1]['numOffers'] == 0:
@@ -19,13 +32,20 @@ with open(offerJsonFile, 'r') as f:
             continue
         # if the last offer is equal to one then send a telegram message to the trends group
         if offers[-1]['numOffers'] == 1:
-            # send a telegram message to the trends group
+            # send a telegram message to the trends group            
             print('only one seller left for: ', legosetid)
-        if (offers[-1] - offers[-2]) > 3:
+            resultMessage.append('only one seller left for: ' + legosetid)
+        if (offers[-1]['numOffers'] - offers[-2]['numOffers']) >= 3:
+            resultMessage.append('This legoset has sold out from more than 3 sellers since last checked: ' + legosetid)
             print('This legoset has sold out from more than 3 sellers since last checked: ', legosetid)
         if(len(offers) >= 3):
-            if (offers[-1] > offers[-2] and offers[-2] > offers[-3]  ):
+            if (offers[-1]['numOffers'] > offers[-2]['numOffers'] and offers[-2]['numOffers'] > offers[-3]['numOffers']  ):
+                resultMessage.append('the last 3 trend checks shows fewer sellers for' + legosetid)
                 print('the last 3 trend checks shows fewer sellers for', legosetid )
+with client:
+    client.loop.run_until_complete(sendResultMessage("\n".join(resultMessage)))
+
+
 
 
              
